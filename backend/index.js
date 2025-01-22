116e756a746e7243d68a30b5f2341ae876402458
Jonathan Zhou <jonathan@Jonathans-MacBook-Pro-3.local>
2025-01-22 16:39:26 -0500

    displaying gif hosted from backend

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/gifs', express.static('public'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
