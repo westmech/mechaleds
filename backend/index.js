bc26aee200d1d5fa29aa61035ad60be840664b93
Jonathan Zhou <jonathanzhou8@gmail.com>
2025-01-23 11:19:29 -0500

    working!!

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
-const express = require('express')
+
+const express  = require("express");
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+
 const app = express()
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.json())
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.get("/prairies", (req, res) =>{
+    io.emit("led", "http://localhost:3000/gifs/prairies.gif");
+    res.send("sent prairies to led");
 })
 
-app.use(express.json())
+app.get("/rockies", (req, res) => {
+    io.emit("led", "http://localhost:3000/gifs/rockies.gif");
+    res.send("sent rockies to led");
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 
 app.post('/rockies', (req, res) => {
   console.log("req", req.body)
@@ -15,6 +42,6 @@ app.post('/rockies', (req, res) => {
 
 app.use('/gifs', express.static('public'))
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
