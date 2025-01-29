89ac48d59900b316a8626c5010f01418055424d1
Jonathan Zhou <jonathanzhou8@gmail.com>
2025-01-28 23:46:28 -0500

    updated frontend. frontend is my passion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
-import {useState} from 'react'
-import { socket } from './socket';
+import styled from 'styled-components'
+import logo from '../public/logo.svg'
 
+const StyledTitle = styled.div`
+  margin-top: 500px;
+  font-size:  100px;
+`;
+
+const StyledImg = styled.img`
+    width: 300px;
+`;
+
+const StyledLinks = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 100px;
+    font-size: 40px;
+    background: white;
+`;
 
 import { Link } from "react-router";
 
@@ -10,15 +26,16 @@ function App() {
   return (
   <>
     <div>
-        Home page mecha mayhem led screen
-        <div style={{display: "flex", justifyContent: "space-between", marginTop: "100px"}}>
+      <StyledTitle>mecha mayhem led screens</StyledTitle>
+      <StyledImg src={logo} />
+        <StyledLinks>
           <Link to ="/dome"> dome </Link>
           <Link to ="/prairies"> prairies (HS) </Link>
           <Link to ="/rockies"> rockies (HS) </Link>
           <Link to ="/badlands"> badlands (VEXU) </Link>
           <Link to ="/foothills"> foothills (MS) </Link>
 
-        </div>
+        </StyledLinks>
     </div>
   </>
   )
--- frontend/src/components/Prematch.jsx
+++ frontend/src/components/Prematch.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
+import mqtt from 'mqtt'
 
 const StyledLed = styled.div`
     width: 1176px;
     height: 168px;
-    background: aqua;
+    background-image: url("/prematch.png");
+`;
+
+const StyledPrematch = styled(StyledLed)`
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr ;
+`;
+
+const StyledTitle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: red;
+    font-size: 100px;
 `;
 
 const Prematch =  () => {
 
+  const client = mqtt.connect("wss://test.mosquitto.org:8081");
+  client.on("connect", () => {});
+
+
   return(
-    <StyledLed>
-      hello
-    </StyledLed>
+    <StyledPrematch>
+      <h1>210Y</h1>
+      <StyledTitle>UP NEXT Q1</StyledTitle>
+      <h1>210y</h1>
+    </StyledPrematch>
   );
 
 }
